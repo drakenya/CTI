6e8e13d9ff2fac9c45039abd0819ef831d1c5722
Andrew Kroll <krolla@gmail.com>
2019-01-12 18:48:55 -0500

    Testing Code

---
+++ Program Board Testing TCL code/Block Power and Control Board (style #2).tcl
@@ -0,0 +1,38 @@
+' -  Block Power and Control Board (Style #2)
+
+
+' -  Dash8 1 (8 control)	
+' -  TrainBrain 1 (4 control, 4 sensor (TrainBrain module Sensors not used on this control board))
+' -  Watchman 1 (8 sensor)
+
+'
+CONTROLS:
+    Block_x1_Power_1or2 'Dash-8 1, Port 1
+    Block_x2_Power_1or2 'Dash-8 1, Port 2
+    Block_x3_Power_1or2 'Dash-8 1, Port 3
+    Block_x4_Power_1or2 'Dash-8 1, Port 4
+    Block_x1_Power_12or34 'Dash-8 1, port 5
+    Block_x2_Power_12or34 'Dash-8 1, port 6
+    Block_x3_Power_12or34 'Dash-8 1, port 7
+    Block_x4_Power_12or34 'Dash-8 1, port 8
+
+    Block_x1_Power_3or4 'TrainBrain 1, Port 1
+    Block_x2_Power_3or4 'TrainBrain 1, Port 2
+    Block_x3_Power_3or4 'TrainBrain 1, Port 3
+    Block_x4_Power_3or4 'TrainBrain 1, Port 4
+
+SENSORS:
+    Block_SPARE1_East_Sensor# 'TrainBrain 1, Port 1
+    Block_sPARE2_West_Sensor# 'TrainBrain 1, Port 2
+    Block_sPARE3_East_Sensor# 'TrainBrain 1, Port 3
+    Block_sPARE4_West_Sensor# 'TrainBrain 1, Port 4
+
+    Block_x1_East_Sensor# 'Watchman 1, Port 1
+    Block_x1_West_Sensor# 'Watchman 1, Port 2
+    Block_x2_East_Sensor# 'Watchman 1, Port 3
+    Block_x2_West_Sensor# 'Watchman 1, Port 4
+    Block_x3_East_Sensor# 'Watchman 1, Port 5
+    Block_x3_West_Sensor# 'Watchman 1, Port 6
+    Block_x4_East_Sensor# 'Watchman 1, Port 7
+    Block_x4_West_Sensor# 'Watchman 1, Port 8
+
